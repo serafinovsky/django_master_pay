e3edb847129ae744c7407a05fe4d448ebed3e591
serafinovsky <m.serafinovskiy@dev.kokoc.com>
2024-04-10 21:23:40 +0700

    Django 4 fix

--- django_master_pay/models/payment.py
+++ django_master_pay/models/payment.py
@@ -1,7 +1,7 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Payment(models.Model):
